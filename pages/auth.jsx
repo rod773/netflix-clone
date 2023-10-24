7f9a7d14ac647599ea9dcd5a3584a10f421f1b37
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-24 16:45:58 +0200

    logo and bg images

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,5 +1,16 @@
 const Auth = () => {
-  return <div>auth</div>;
+  return (
+    <div
+      className="relative h-full w-full bg-[url('/images/hero.jpg')]
+       bg-no-repeat bg-center bg-cover"
+    >
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
